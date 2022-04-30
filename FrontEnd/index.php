d85af86c7d5e669fdff1c622d2c74ce5e418e29f
jdanielsuarez98 <jdanielsuarez98@users.noreply.github.com>
2022-04-30 15:31:07 -0500

    BOOTSTRAP

--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -9,7 +9,25 @@
     <link rel="stylesheet" href="../css/login.css">
     <link rel="stylesheet" href="../css/cabecera.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-   
+    <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="https://www.valoraanalitik.com/wp-content/uploads/2018/03/BancodeBogota-696x461.jpg"
+         width="100" height="300" class="d-inline-block align-top" alt="">
+        Bootstrap
+        <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-link">Link</button>
+    </a>
+    </nav>
+
 </body>
