4d5b0a496fc781c284e884eb2b8a11004c707cd2
DanielMorales2000 <65742760+DanielMorales2000@users.noreply.github.com>
2022-05-18 16:06:30 -0500

    .

---
+++ FrontEnd/info.php
@@ -0,0 +1,16 @@
+
+<?php 
+
+
+infophp();
+
+ ini_set('display_errors', 1);
+ ini_set('display_startup_errors', 1);
+ error_reporting(E_ALL);
+ session_start();
+ var_dump($_SESSION);
+ exit();
+
+
+ 
+?>
