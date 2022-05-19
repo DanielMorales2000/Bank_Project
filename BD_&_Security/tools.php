8a1ffe4fe883140fdb1e7afd4d4f036b745094e5
DanielMorales2000 <65742760+DanielMorales2000@users.noreply.github.com>
2022-05-18 22:29:13 -0500

    .

--- BD_&_Security/tools.php
+++ BD_&_Security/tools.php
@@ -13,7 +13,7 @@
         }
         else if($_SESSION["tip_user"] == null){
             // header("Location: ../FrontEnd/index.front.php");
-            echo '<script> window.location.replace("http://nuevapagina.php/"); </script>';
+            echo '<script> window.location.replace("../FrontEnd/index.front.php"); </script>';
         }
     }
 
