73c0c59c3de2a30c3036fb09a39c7c1f95dd619f
DanielMorales2000 <65742760+DanielMorales2000@users.noreply.github.com>
2022-05-18 23:20:28 -0500

    .

--- FrontEnd/FiltrarCuentaHabientes.front.php
+++ FrontEnd/FiltrarCuentaHabientes.front.php
@@ -1,9 +1,9 @@
  <?php 
+ session_start();
  ini_set('display_errors', 1);
  ini_set('display_startup_errors', 1);
  error_reporting(E_ALL);
  include('../BD_&_Security/tools.php');
- session_start();
  LimpiarEntradas();        
  regularNavegacion(2);
 
