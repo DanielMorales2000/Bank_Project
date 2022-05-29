37b294faf07b5e37a57df4c4f6f50fb9c3f74d8f
DanielMorales2000 <65742760+DanielMorales2000@users.noreply.github.com>
2022-05-28 20:52:17 -0500

    .

--- FrontEnd/sendEmail.php
+++ FrontEnd/sendEmail.php
@@ -1,101 +1,51 @@
-<?php ob_start();?>
 <?php
-include('../conexion.php');
-require_once '../vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
-$mpdf = new \Mpdf\Mpdf();
-$stylesheet = file_get_contents('../CSS/style.css');
-$mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
+require '..\vendor\phpmailer\phpmailer\src\Exception.php';
+/* Clase principal de PHPMailer */
+require '..\vendor\phpmailer\phpmailer\src\PHPMailer.php';
+/* Clase SMTP, necesaria si quieres usar SMTP */
+require '..\vendor\phpmailer\phpmailer\src\SMTP.php';
 
-//Primer nivel: CUENTAHABIENTES >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-$cuentaHabientesQuery= "EXEC [dbo].[FILTRAR_CUENTAHABIENTES]";
-$cuentaHabientesResult=sqlsrv_query($conn, $cuentaHabientesQuery);
+require '..\vendor\autoload.php';
+//Create an instance; passing `true` enables exceptions
 
-while($fila = sqlsrv_fetch_array($cuentaHabientesResult)){
-    $nombre = $fila['NOMBRES'];
-    $apellido = $fila['APELLIDOS'];
-    $documento = $fila['DOCUMENTO'];
-    $email = $fila['CORREO'];
 
-    $mpdf->WriteHTML('<h1 style="color:red">Histórico de Movimientos</h1>');
+function sendEmail($mpdf,$email){
+    $mail = new PHPMailer(TRUE);
+    try {
+        //Recibir todos los parámetros del formulario
+        $emailAttachment = $mpdf->Output('','S');
+        //Server settings
+        $mail->SMTPDebug = 0;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.office365.com';      //Set the SMTP server to send through
+        // smtp.office365.com
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'danielsuarez8910@outlook.com';                     //SMTP username
+        $mail->Password   = base64_decode('OTgwNzIw');                               //SMTP password
+        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    
+        //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    $mpdf->WriteHTML('<div style="margin-left:20px" align = "left">
-        <b>Nombre:</b>'.$nombre.'<br>
-        <b>Apellido:</b>'.$apellido.'<br>
-        <b>Documento:</b>'.$documento.'<br>
-        <b>Correo:</b>'.$email.'<br>');
-
-    $mpdf->WriteHTML('_______________________________________________________________________________________________________<br><br>');
-
-//Segundo nivel: CUENTAS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-        $cuentasQuery = "EXEC [dbo].[CUENTAHABIENTE]
-        @DOCUMENTO = N'$documento'";
-        $cuentasResult=sqlsrv_query($conn, $cuentasQuery);
-
-        while($fila = sqlsrv_fetch_array($cuentasResult)){ 
-            $NUM_CUENTA = $fila['NUM_CUENTA'];
-            $TIPO_CUENTA = $fila['TIPO_CUENTA'];
-            $SALDO = $fila['SALDO'];
-
-            $mpdf->WriteHTML('<div style="margin-left:20px" align = "left">
-                <b>Numero de Cuenta:</b>'.$NUM_CUENTA.'<br>
-                <b>Tipo de Cuenta:</b>'.$TIPO_CUENTA.'<br>
-                <b>Saldo Actual:</b>'.$SALDO.'<br>');
-//Tercer nivel: Movimientos >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-            $mpdf->WriteHTML('
-            <table class="blueTable">
-            <thead>
-            <tr align="center">
-                <th width="100" align="center">Fecha</th>
-                <th width="150" align="center">Tipo de Transacción</th>
-                <th width="100" align="center">Valor</th>
-                <th width="150" align="center">Cuenta de Movimiento</th>
-                <th width="150" align="center">Cuenta Destino</th>
-                <th width="300" align="center">Descripción</th>
-            </tr>
-            </thead>
-            <tbody>');
-
-            $transactionsQuery = "EXEC	[dbo].[PA_TRANSACTIONS_HISTORY]
-            @N_CUENTA = N'$NUM_CUENTA'";
-
-            $transactionsResult=sqlsrv_query($conn, $transactionsQuery);
-
-                while($fila = sqlsrv_fetch_array($transactionsResult)){ 
-                    $FECHA = $fila['FECHA'];
-                    $TIPO_TRANSACCION = $fila['TIPO_TRANSACCION'];
-                    $VALOR = $fila['VALOR'];
-                    $DESCRIPCION = $fila['DESCRIPCION'];
-                    $CUENTA_ORIGEN = $fila['CUENTA_ORIGEN'];
-                    $CUENTA_DESTINO = $fila['CUENTA_DESTINO'];
-
-                    $stringDate = $FECHA->format('Y-m-d H:i:s');
-
-                    $mpdf->WriteHTML(
-                        '<tr align="center">
-                        <td align="center">'.$stringDate.'</td>
-                        <td align="center">'.$TIPO_TRANSACCION.'</td>
-                        <td align="center">'.$VALOR.'</td>
-                        <td align="center">'.$CUENTA_ORIGEN.'</td>
-                        <td align="center">'.$CUENTA_DESTINO.'</td>
-                        <td align="center">'.$DESCRIPCION.'</td>
-                        </tr>'
-                    
-                    );
-                }
-            $mpdf->WriteHTML(
-            '</tbody>
-        </table>
-    </div>
-</div>');
-        $mpdf->WriteHTML('_______________________________________________________________________________________________________<br><br>');
+        //Recipients
+        $mail->setFrom('danielsuarez8910@outlook.com', 'Daniel');
+        //Content
+        $mail->AddAddress($email); 
+        $mail->Subject = 'Extracto Bancario';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');
+        $mail->AddStringAttachment($emailAttachment, 'file.pdf', 'base64', 'application/pdf');
+        $mail->send();
+        echo'<script type="text/javascript">
+                alert("Enviado Correctamente");
+            </script>';
         
-        }
-        echo "Enviado";
-    // $mpdf->Output();
+    } catch (Exception $e) {
+        echo $e;
+    }
 }
 ?>
-<?php ob_end_flush(); ?>
 
