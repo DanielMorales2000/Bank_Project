71a295759e68b16bad8b3864393b1cab4c1a4a7e
DanielMorales2000 <65742760+DanielMorales2000@users.noreply.github.com>
2022-05-29 20:06:46 -0500

    .

--- FrontEnd/mainPageBanquero.front.php
+++ FrontEnd/mainPageBanquero.front.php
@@ -1,14 +1,15 @@
 <?php ob_start();?>
 <?php
-// include('./createPDF.php');
+include('./createPDF.php');
 include('../BackEnd/mainPage.back.php');
-// regularNavegacion(2);
+
+regularNavegacion(2);
 if(isset($_POST['CloseSession'])){
     closeSession();
 }
-// if(isset($_POST['GenerateReports'])){
-//     GenerateReports($conn);
-// }
+if(isset($_POST['GenerateReports'])){
+    GenerateReports($conn);
+}
 ?>
 <?php ob_end_flush(); ?>
 
@@ -34,11 +35,13 @@ if(isset($_POST['CloseSession'])){
     </a>
     <a class="navbar-brand">
         <form method="POST">  
+            <input type="hidden" name="anticsrf" value="<?php echo $_SESSION['anticsrf'];?>">  
             <button type="submit" class="btn btn-primary" name="GenerateReports">Generar Reportes</button>
         </form>
     </a>
     <a class="navbar-brand">
         <form method="POST">   
+            <input type="hidden" name="anticsrf" value="<?php echo $_SESSION['anticsrf'];?>">  
             <button type="submit" class="btn btn-link" name="CloseSession">Cerrar Sesión</button>
         </form>
     </a>
@@ -48,7 +51,9 @@ if(isset($_POST['CloseSession'])){
         <H1>BIENVENIDOS AL BANCO UDEC</H1>
         <!-- <img src="https://www.valoraanalitik.com/wp-content/uploads/2018/03/BancodeBogota-696x461.jpg" -->
             <!-- width="600" height="400" class="d-inline-block align-top" alt=""> -->
-
+        <?php
+        seeData($conn);
+        ?>
             
     </div>
 </body>
