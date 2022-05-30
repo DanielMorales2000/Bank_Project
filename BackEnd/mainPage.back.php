e405c32d759e4a8deddabe0ceba83b96c8462be7
DanielMorales2000 <65742760+DanielMorales2000@users.noreply.github.com>
2022-05-29 20:06:40 -0500

    -

--- BackEnd/mainPage.back.php
+++ BackEnd/mainPage.back.php
@@ -1,8 +1,9 @@
 <?php ob_start();?>
 <?php 
 include('../BD_&_Security/tools.php');
-IniciarSesionSegura();
 include('../conexion.php');
+MostrarErrores();
+IniciarSesionSegura();
 GenerarAntiCSRF();
 
 function seeData($conn){
@@ -14,17 +15,16 @@ function seeData($conn){
   $resultado=sqlsrv_query($conn, $consulta);
   $filas=sqlsrv_fetch_array($resultado);
 
-  // if($filas){
-  //   echo "<b>BANCO:  </b>".$filas[0]."<br>";
-  //   echo "<b>NIT:  </b>".$filas[1]."<br>";
-  //   echo "<b>CIUDAD:  </b>".$filas[2]."<br>";
-  //   echo "<b>SEDE:  </b>".$filas[3]."<br>";
-  //   echo "<b>TELEFONO:  </b>".$filas[4]."<br>";
-  //   echo "<b>GERENTE:  </b>".$filas[5]."<br>";
-  //   $_SESSION["id_sucursal"] = $filas[6];
-  // }
+  if($filas){
+    echo "<b>BANCO:  </b>".$filas[0]."<br>";
+    echo "<b>NIT:  </b>".$filas[1]."<br>";
+    echo "<b>CIUDAD:  </b>".$filas[2]."<br>";
+    echo "<b>SEDE:  </b>".$filas[3]."<br>";
+    echo "<b>TELEFONO:  </b>".$filas[4]."<br>";
+    echo "<b>GERENTE:  </b>".$filas[5]."<br>";
+    $_SESSION["id_sucursal"] = $filas[6];
+  }
 }
-
 
 ?>
 <?php ob_end_flush(); ?>
